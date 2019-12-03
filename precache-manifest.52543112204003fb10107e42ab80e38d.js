c71437b7368ae756e8619a07f1c36c3b127c1a55
Andrew Baker <andrewbaker@andrews-mbp.home>
2019-12-02 22:06:35 -0500

    Updates

---
+++ precache-manifest.52543112204003fb10107e42ab80e38d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9188c050e148697e8abb20920be8004",
+    "url": "/marbles/index.html"
+  },
+  {
+    "revision": "66a4784079c1a79c5620",
+    "url": "/marbles/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e425d46aa2b2db000238",
+    "url": "/marbles/static/js/2.0d15df05.chunk.js"
+  },
+  {
+    "revision": "66a4784079c1a79c5620",
+    "url": "/marbles/static/js/main.ac8b42ae.chunk.js"
+  },
+  {
+    "revision": "b099a1cad3a925107a96",
+    "url": "/marbles/static/js/runtime-main.ad181a6e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/marbles/static/media/logo.25bf045c.svg"
+  }
+]);
